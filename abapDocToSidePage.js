8fbd574d39f2168ad3c98d1e4b15961ff95e765a
zzfancitizen@gmail.com <zz64132332>
2019-02-26 00:40:42 +0800

    Move abac document to side page

--- abapDocToSidePage.js
+++ abapDocToSidePage.js
@@ -1,6 +1,6 @@
 const fsFacadeDefiner = require('./fsFacad');
 
-
+if (process.argv.length < 4) throw 'Please input package name & sidePage path';
 let fsFacade = new fsFacadeDefiner(process.argv[process.argv.length - 2]);
 fsFacade.writeObjectToFile(process.argv[process.argv.length - 1]);
 
